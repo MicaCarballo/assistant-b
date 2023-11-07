00a12f12513eab0ac6b35d7ee74cde8153846f4e
MicaCarballo <micacarballo2@gmail.com>
2023-11-07 09:15:48 -0800

    added render to CORS

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ origins = [
     "http://localhost:5174",
     "http://localhost:4173",
     "http://localhost:3000",
-    "http://localhost:8000"
+    "http://localhost:8000",
+     "https://assistant-backend.onrender.com"
 ]
 #Cors- Middleware
 app.add_middleware(
